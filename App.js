2af4a4bec7b6dd2204da3065b0211e3ccf05de68
Eda Belge <edabelge00@gmail.com>
2023-08-28 13:54:58 +0300

    Navigating from All places to Add places added

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/UI/IconButton";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,11 +13,12 @@ export default function App() {
       <StatusBar style='dark' />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name='AllPlaces' component={AllPlaces} />
+          <Stack.Screen name='AllPlaces' component={AllPlaces} options={({ navigation }) => ({
+            headerRight: ({ tintColor }) => (<IconButton icon="add" size={24} color={tintColor} onPress={() => navigation.navigate("AddPlace")} />),
+          })} />
           <Stack.Screen name='AddPlace' component={AddPlace} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
   );
 }
-
