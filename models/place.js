b595c4aea1964c618cdf6529273c748bbb43a60e
Eda Belge <edabelge00@gmail.com>
2023-08-28 20:09:18 +0300

    Place's address updated

--- models/place.js
+++ models/place.js
@@ -1,8 +1,8 @@
 class Place {
-    constructor({ title, imageUri, address, location }) {
+    constructor({ title, imageUri, location }) {
         this.title = title;
         this.imageUri = imageUri;
-        this.address = address;
+        this.address = location.address;
         this.location = location;
         this.id = new Date().toString() + Math.random().toString();
     }
