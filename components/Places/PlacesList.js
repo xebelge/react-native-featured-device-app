9623bdf45bda49d3239b0325c2711c2eca53b8fe
Eda Belge <edabelge00@gmail.com>
2023-08-28 14:22:55 +0300

    Text color changed for Places List

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,5 +1,6 @@
 import { FlatList, View, Text, StyleSheet } from "react-native";
 import PlaceItem from "./PlaceItem";
+import { Colors } from '../../constants/color';
 
 function PlacesList({ places }) {
 
@@ -21,6 +22,7 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
     fallbackText: {
-        fontSize: 16
+        fontSize: 16,
+        color: Colors.primary200
     }
 })
