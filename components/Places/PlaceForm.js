6bde978f7a403ac84eceadc612dea7258bac5cce
Eda Belge <edabelge00@gmail.com>
2023-08-28 13:30:02 +0300

    Place form component added

---
+++ components/Places/PlaceForm.js
@@ -0,0 +1,7 @@
+function PlaceForm() {
+    return <View>
+        <Text>Place Form</Text>
+    </View>
+}
+
+export default PlaceForm;
--- screens/AddPlace.js
+++ screens/AddPlace.js
@@ -1,5 +1,8 @@
-function AddPlace() {
+import { View, Text } from "react-native";
+import PlaceForm from "../components/Places/PlaceForm";
 
+function AddPlace() {
+    return <PlaceForm />
 }
 
 export default AddPlace;
