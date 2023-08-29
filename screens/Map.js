ab76132a545bdb49dc84e6fa42d2cbc06a547a2e
Eda Belge <edabelge00@gmail.com>
2023-08-29 13:56:46 +0300

    Refactor Map component to handle initial location and selected location

--- screens/Map.js
+++ screens/Map.js
@@ -4,18 +4,24 @@ import { useCallback, useLayoutEffect, useState } from "react";
 import { Alert } from "react-native";
 import IconButton from "../components/UI/IconButton";
 
-function Map({ navigation }) {
+function Map({ navigation, route }) {
 
-    const [selectedLocation, setSelectedLocation] = useState();
+    const initialLocation = route.params && { lat: route.params.initialLat, lng: route.params.InitialLng };
+
+    const [selectedLocation, setSelectedLocation] = useState(initialLocation);
 
     const region = {
-        latitude: 37.78,
-        longitude: -122.43,
+        latitude: initialLocation ? initialLocation : 37.78,
+        longitude: initialLocation ? initialLocation : -122.43,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
     }
 
     function selectLocationHandler(event) {
+        if (initialLocation) {
+            return;
+        }
+
         const lat = event.nativeEvent.coordinate.latitude;
         const lng = event.nativeEvent.coordinate.longitude;
 
@@ -32,10 +38,14 @@ function Map({ navigation }) {
     }, [navigation, selectedLocation]);
 
     useLayoutEffect(() => {
+        if (initialLocation) {
+            return;
+        }
+
         navigation.setOptions({
             headerRight: ({ tintColor }) => <IconButton icon="save" size={24} color={tintColor} onPress={savePickedLocationHandler} />
         });
-    }, [navigation, savePickedLocationHandler]);
+    }, [navigation, savePickedLocationHandler, initialLocation]);
 
     return (
         <MapView style={styles.map} initialRegion={region} onPress={selectLocationHandler}>
