738a9a8811f79710a673143d18b53b8e31dad0e4
Eda Belge <edabelge00@gmail.com>
2023-08-29 11:20:58 +0300

    Update all places page related fetching data

--- screens/AllPlaces.js
+++ screens/AllPlaces.js
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
 import PlacesList from "../components/Places/PlacesList";
 import { useIsFocused } from "@react-navigation/native";
+import { fetchPlaces } from "../util/database";
 
 function AllPlaces({ route }) {
     const isFocused = useIsFocused();
     const [loadedPlaces, setLoadedPlaces] = useState();
 
     useEffect(() => {
-        if (isFocused && route.params) {
-            setLoadedPlaces((currentPlaces) => [...currentPlaces, route.params.place]);
+        async function loadPlaces() {
+            const places = await fetchPlaces();
+            setLoadedPlaces(places);
         }
-    }, [isFocused, route]);
+
+        if (isFocused) {
+            loadPlaces();
+            // setLoadedPlaces((currentPlaces) => [...currentPlaces, route.params.place]);
+        }
+    }, [isFocused]);
 
     return <PlacesList places={loadedPlaces} />
 }
