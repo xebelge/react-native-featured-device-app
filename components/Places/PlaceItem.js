e0e2f7e97ff8e89421c0871a730bf01f0f4cf1a6
Eda Belge <edabelge00@gmail.com>
2023-08-29 11:40:30 +0300

    Add Place handler

--- components/Places/PlaceItem.js
+++ components/Places/PlaceItem.js
@@ -3,7 +3,7 @@ import { Colors } from '../../constants/color';
 
 function PlaceItem({ place, onSelect }) {
     return <Pressable style={({ pressed }) => [styles.item, pressed && styles.pressed]}
-        onPress={onSelect} >
+        onPress={onSelect.bind(this, place.id)} >
         <Image style={styles.image} source={{ uri: place.imageUri }} />
         <View style={styles.info} >
             <Text style={styles.title} >{place.title}</Text>
--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,8 +1,17 @@
 import { FlatList, View, Text, StyleSheet } from "react-native";
 import PlaceItem from "./PlaceItem";
 import { Colors } from '../../constants/color';
+import { useNavigation } from "@react-navigation/native";
 
 function PlacesList({ places }) {
+
+    const navigation = useNavigation();
+
+    function selectPlaceHandler() {
+        navigation.navigate("PlaceDetails", {
+            placeId: id
+        })
+    }
 
     if (!places || places.length === 0) {
         return <View style={styles.fallbackContainer}>
@@ -10,7 +19,7 @@ function PlacesList({ places }) {
         </View>
     }
 
-    return <FlatList style={styles.list} data={places} keyExtractor={(item) => item.id} renderItem={({ item }) => <PlaceItem place={item} />} />;
+    return <FlatList style={styles.list} data={places} keyExtractor={(item) => item.id} renderItem={({ item }) => <PlaceItem place={item} onSelect={selectPlaceHandler} />} />;
 }
 
 export default PlacesList;
